aeb8632fb6f96d879d5d1cd2ff316bf8528a0e4f
Zac Sweers <pandanomic@gmail.com>
2025-11-03 15:03:58 -0500

    Formatting

--- compiler-tests/src/test/data/box/interop/dagger/OptionalInNestedGraphExtensionCanBeLoaded.kt
+++ compiler-tests/src/test/data/box/interop/dagger/OptionalInNestedGraphExtensionCanBeLoaded.kt
@@ -4,55 +4,57 @@ import java.util.Optional
 import kotlin.jvm.optionals.getOrDefault
 
 interface LoggedInScope
+
 interface FeatureScope
 
 interface DelegateDependency
 
 @ContributesBinding(AppScope::class)
-class DelegateDependencyImpl @Inject constructor(
-    private val appDependency: AppDependency,
-    private val LoggedInDependency: Optional<LoggedInDependency>
-): DelegateDependency by LoggedInDependency.getOrDefault(appDependency)
+class DelegateDependencyImpl
+@Inject
+constructor(
+  private val appDependency: AppDependency,
+  private val LoggedInDependency: Optional<LoggedInDependency>,
+) : DelegateDependency by LoggedInDependency.getOrDefault(appDependency)
 
 interface AppDependency : DelegateDependency
 
 @ContributesBinding(AppScope::class)
 @SingleIn(AppScope::class)
-class AppDependencyImpl @Inject constructor(): AppDependency
+class AppDependencyImpl @Inject constructor() : AppDependency
 
 interface LoggedInDependency : DelegateDependency
 
 @ContributesBinding(LoggedInScope::class)
 @SingleIn(LoggedInScope::class)
-class LoggedInDependencyImpl @Inject constructor(): LoggedInDependency
+class LoggedInDependencyImpl @Inject constructor() : LoggedInDependency
 
 @dagger.Module
 @ContributesTo(AppScope::class)
 interface DependencyModule {
-    @dagger.BindsOptionalOf
-    fun provideOptional(): LoggedInDependency
+  @dagger.BindsOptionalOf fun provideOptional(): LoggedInDependency
 }
 
 @SingleIn(FeatureScope::class)
 @GraphExtension(FeatureScope::class)
 interface FeatureGraph {
-    val dependency: DelegateDependency
+  val dependency: DelegateDependency
 }
 
 @SingleIn(LoggedInScope::class)
 @GraphExtension(LoggedInScope::class)
 interface LoggedInGraph {
-    val featureGraph: FeatureGraph
+  val featureGraph: FeatureGraph
 }
 
 @SingleIn(AppScope::class)
 @DependencyGraph(AppScope::class)
 interface AppGraph {
-    val loggedInGraph: LoggedInGraph
+  val loggedInGraph: LoggedInGraph
 }
 
 fun box(): String {
-    val graph = createGraph<AppGraph>()
-    assertNotNull(graph.loggedInGraph.featureGraph.dependency)
-    return "OK"
+  val graph = createGraph<AppGraph>()
+  assertNotNull(graph.loggedInGraph.featureGraph.dependency)
+  return "OK"
 }
