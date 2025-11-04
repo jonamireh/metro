5192df81a4a524176a42cea1a78ee03ece54f523
Jon Amireh <jonamireh@dropbox.com>
2025-11-04 13:58:54 -0500

    Add failing test for #1320

---
+++ compiler-tests/src/test/data/box/interop/dagger/ScopedProvidesProviderCanBeLoaded.kt
@@ -0,0 +1,31 @@
+// ENABLE_DAGGER_INTEROP
+package test
+
+import javax.inject.Provider
+
+@SingleIn(AppScope::class)
+@DependencyGraph(AppScope::class)
+interface AppGraph
+
+interface Dependency
+
+@dagger.Module
+@ContributesTo(AppScope::class)
+interface DependencyModule {
+    companion object {
+        @dagger.Provides
+        @SingleIn(AppScope::class)
+        fun provideDependency() : Dependency = object: Dependency { }
+    }
+}
+
+@ContributesTo(AppScope::class)
+interface DependencyProvider {
+    val provider: Provider<Dependency>
+}
+
+fun box(): String {
+    val graph = createGraph<AppGraph>()
+    assertNotNull(graph.asContribution<DependencyProvider>().provider.get())
+    return "OK"
+}
