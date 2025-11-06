a844bf3d09351e1655dbc5609ae195e7ee59727a
Zac Sweers <pandanomic@gmail.com>
2025-11-06 17:47:39 -0500

    Simplify

--- compiler-tests/src/test/data/box/dependencygraph/extensions/FactoryCanBeInjectedInProvider.kt
+++ compiler-tests/src/test/data/box/dependencygraph/extensions/FactoryCanBeInjectedInProvider.kt
@@ -1,6 +1,10 @@
+// https://github.com/ZacSweers/metro/issues/1331
+// Namely this ensures that a parent-unused, unscoped binding referenced by a child graph still gets
+// a property collected
+
 @DependencyGraph(AppScope::class)
 interface AppGraph {
-    val loggedInGraph: LoggedInGraph
+  val loggedInGraph: LoggedInGraph
 }
 
 interface LoggedInScope
@@ -8,42 +12,33 @@ interface LoggedInScope
 @SingleIn(LoggedInScope::class)
 @GraphExtension(LoggedInScope::class)
 interface LoggedInGraph {
-    val loggedInFeatureInteractor: FeatureInteractor
+  val loggedInFeatureInteractor: FeatureInteractor
+
+  @Provides
+  @SingleIn(LoggedInScope::class)
+  fun provideLoggedInFeatureInteractor(factory: FeatureGraph.Factory): FeatureInteractor =
+    object : FeatureInteractor {}.also { factory.createFeatureGraph() }
 }
 
 interface FeatureScope
 
-@SingleIn(FeatureScope::class)
 @GraphExtension(FeatureScope::class)
 interface FeatureGraph {
-    @GraphExtension.Factory
-    interface Factory {
-        fun createFeatureGraph(): FeatureGraph
-    }
+  @GraphExtension.Factory
+  interface Factory {
+    fun createFeatureGraph(): FeatureGraph
+  }
 
-    @ContributesTo(AppScope::class)
-    interface ParentBindings {
-        fun featureFactory(): Factory
-    }
+  @ContributesTo(AppScope::class)
+  interface ParentBindings {
+    fun featureFactory(): Factory
+  }
 }
 
 interface FeatureInteractor
 
-@ContributesTo(LoggedInScope::class)
-interface LoggedInFeatureModule {
-    companion object {
-        @Provides
-        @SingleIn(LoggedInScope::class)
-        fun provideLoggedInFeatureInteractor(
-            factory: FeatureGraph.Factory
-        ): FeatureInteractor = object : FeatureInteractor {}.also {
-            factory.createFeatureGraph()
-        }
-    }
-}
-
 fun box(): String {
-    val appGraph = createGraph<AppGraph>()
-    assertNotNull(appGraph)
-    return "OK"
+  val appGraph = createGraph<AppGraph>()
+  assertNotNull(appGraph)
+  return "OK"
 }
