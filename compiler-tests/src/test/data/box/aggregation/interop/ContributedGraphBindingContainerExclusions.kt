3d7b4cceddc50c91c1ea4227b784a014b1d05eab
Zac Sweers <pandanomic@gmail.com>
2025-11-25 21:12:45 -0500

    More tests for extensions/subcomponents

--- compiler-tests/src/test/data/box/aggregation/interop/ContributedGraphBindingContainerExclusions.kt
+++ compiler-tests/src/test/data/box/aggregation/interop/ContributedGraphBindingContainerExclusions.kt
@@ -3,6 +3,7 @@
 
 import com.squareup.anvil.annotations.ContributesSubcomponent
 import com.squareup.anvil.annotations.MergeComponent
+import com.squareup.anvil.annotations.MergeSubcomponent
 import dagger.Module
 
 @ContributesSubcomponent(String::class, parentScope = AppScope::class, exclude = [IntBinding1::class])
@@ -17,7 +18,9 @@ interface StringGraph {
 }
 
 @MergeComponent(AppScope::class)
-interface AppGraph
+interface AppGraph {
+    val unitGraph: UnitGraph
+}
 
 @ContributesTo(String::class)
 @Module
@@ -31,9 +34,28 @@ object IntBinding2 {
   @Provides fun provideInt(): Int = 2
 }
 
+// Test MergeSubcomponent with exclude
+@MergeSubcomponent(Unit::class, exclude = [LongBinding1::class])
+interface UnitGraph {
+    val long: Long
+}
+
+@ContributesTo(Unit::class)
+@Module
+object LongBinding1 {
+  @Provides fun provideLong(): Long = 1L
+}
+
+@ContributesTo(Unit::class)
+@Module
+object LongBinding2 {
+  @Provides fun provideLong(): Long = 2L
+}
+
 fun box(): String {
   val graph = createGraph<AppGraph>()
   val stringGraph = graph.asContribution<StringGraph.Factory>().create()
   assertEquals(2, stringGraph.int)
+  assertEquals(2L, graph.unitGraph.long)
   return "OK"
 }
